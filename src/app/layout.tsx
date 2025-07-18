abeca73ea785333c31e92bbd6434b18e0a5379bf
Enrique Suarez <enriquesuarezmartin@gmail.com>
2025-07-18 11:18:27 +0100

    navbarAddedToLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import { Providers } from '@/components/molecules/providers'
+import { Providers } from '@/components/molecules/providers/providers'
+import Navbar from '@/components/navbar/navbar'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         <Providers>{children}</Providers>
       </body>
     </html>
