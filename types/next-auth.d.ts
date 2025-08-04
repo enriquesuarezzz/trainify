56a01d0f27da1c4f54f1e3f5732ca1dd5e9d8039
Enrique Suarez <enriquesuarezmartin@gmail.com>
2025-08-04 10:20:12 +0100

    next-authUpdate

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -7,6 +7,11 @@ declare module 'next-auth' {
       name?: string | null
       email?: string | null
       image?: string | null
+      role: 'user' | 'admin'
     }
+  }
+  interface User {
+    id: string
+    role?: 'admin' | 'user'
   }
 }
